db7e12ca410d73a7d756a4ff5aa2f8d5ce633959
DeHoKo <zenkovsk@gmail.com>
2021-01-14 18:22:54 +0300

    feat: add footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      marginTop: 'auto',
+      backgroundColor: theme.palette.background.paper,
+    },
+    horizontal: {
+      display: 'flex',
+    },
+    iconWidth: {
+      minWidth: '32px',
+    },
+  }),
+);
+
+function ListItemLink(props: ListItemProps<'a', { button?: true }>) {
+  return <ListItem button component="a" {...props} />;
+}
 
 function Footer() {
-  return (
-    <footer>
+  const classes = useStyles();
 
+  return (
+    <footer className={classes.root}>
+      <Container maxWidth="sm">
+        <List className={classes.horizontal}  dense={true} >
+          <ListItemLink href="https://github.com/vladstepway" target="_blank" rel="noopener">
+            <ListItemIcon className={classes.iconWidth}>
+              <GitHubIcon fontSize='small' />
+            </ListItemIcon>
+            <ListItemText primary="vlad" />
+          </ListItemLink>
+          <ListItemLink href="https://github.com/dehoko" target="_blank" rel="noopener">
+            <ListItemIcon className={classes.iconWidth}>
+              <GitHubIcon fontSize='small' />
+            </ListItemIcon>
+            <ListItemText primary="dehoko" />
+          </ListItemLink>
+          <ListItemLink href="https://github.com/desudesu410" target="_blank" rel="noopener">
+            <ListItemIcon className={classes.iconWidth}>
+              <GitHubIcon fontSize='small' />
+            </ListItemIcon>
+            <ListItemText primary="desu" />
+          </ListItemLink>
+        </List>
+      </Container>
     </footer>
   );
 }
