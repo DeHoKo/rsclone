2b8754f81d2a89f06087e3047229ad774599e93d
DeHoKo <zenkovsk@gmail.com>
2021-01-14 16:13:13 +0300

    init: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from './components/header/header';
+import Main from './components/main/main';
+import Footer from './components/footer/footer';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Main />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
