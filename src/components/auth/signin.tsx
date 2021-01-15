28f336db8a0502bb281874d2368aa5adf1a60a60
DeHoKo <zenkovsk@gmail.com>
2021-01-16 02:48:43 +0300

    feat: add signin

---
+++ src/components/auth/signin.tsx
@@ -0,0 +1,122 @@
+import React, { useRef, useContext } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
+
+import { useHttpClient } from '../../hooks/http-hook';
+import { AuthContext } from '../../context/auth-context';
+
+const REQUEST_URL = 'http://localhost:5000/api/users/login';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+function SignIn() {
+  const classes = useStyles();
+
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const { sendRequest } = useHttpClient();
+  const formEl = useRef<null | HTMLFormElement>(null);
+
+  const signinHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const responseData = await sendRequest(
+        REQUEST_URL,
+        'POST',
+        JSON.stringify({
+          email: formEl?.current?.email?.value,
+          password: formEl?.current?.password?.value,
+        }),
+        {
+          'Content-Type': 'application/json'
+        }
+      );
+      auth.login(responseData.userId, responseData.token, responseData.email);
+      history.push('/');
+    } catch (err) {}
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate ref={formEl} onSubmit={signinHandler}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container justify='flex-end'>
+            <Grid item>
+              <Link variant="body2" component={ RouterLink } to='signup'>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
+
+export default SignIn;
