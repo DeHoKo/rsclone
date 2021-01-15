574fab48e78bcba711071b4038a862bb372412e7
DeHoKo <zenkovsk@gmail.com>
2021-01-15 18:43:08 +0300

    feat: add auth-hook

---
+++ src/hooks/auth-hook.tsx
@@ -0,0 +1,60 @@
+import { useState, useCallback, useEffect } from 'react';
+
+const EXPIRATION_TIME = 1000 * 60 * 60;
+
+let logoutTimer: ReturnType<typeof setTimeout>;
+
+export const useAuth = () => {
+  const [token, setToken] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState<null | Date>(null);
+  const [userId, setUserId] = useState(null);
+  const [userEmail, setUserEmail] = useState(null);
+
+  const login = useCallback((uid, token, uemail, expirationDate) => {
+    setToken(token);
+    setUserId(uid);
+    setUserEmail(uemail)
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + EXPIRATION_TIME);
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userEmail: userEmail,
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setTokenExpirationDate(null);
+    setUserId(null);
+    setUserEmail(null);
+    localStorage.removeItem('userData');
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData') as string);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.userId, storedData.token, new Date(storedData.expiration), storedData.userEmail);
+    }
+  }, [login]);
+
+  return { token, login, logout, userId, userEmail };
+};
