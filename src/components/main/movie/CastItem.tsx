6625f30f55dfc300e2a4c03fcff912dcf9cd546d
ustsepavy <vladstepovoyvios@gmail.com>
2021-01-16 19:07:29 +0300

    feat: added movie component component

---
+++ src/components/main/movie/CastItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Card, CardActionArea, CardContent, CardMedia, Typography} from "@material-ui/core";
+
+type CastItemType = {
+    classes: {
+        [style: string]: string
+    },
+    image: string,
+    title: string,
+    subtitle: string
+}
+
+export default function CastItem({classes, image, title, subtitle}: CastItemType) {
+    return (
+        <div>
+            <CardActionArea className={classes.actionArea}>
+                <Card className={classes.card}>
+                    <CardMedia className={classes.cardImage} component={'img'} image={image}/>
+                    <CardContent className={classes.content}>
+                        <Typography className={classes.title} variant={'h2'}>
+                            {title}
+                        </Typography>
+                        <Typography className={classes.subtitle}>{subtitle}</Typography>
+                    </CardContent>
+                </Card>
+            </CardActionArea>
+        </div>
+    );
+};
