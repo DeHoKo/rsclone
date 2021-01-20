80cc0c82173ab4ec2da3ef747b69148c146173ff
ustsepavy <vladstepovoyvios@gmail.com>
2021-01-20 23:55:57 +0300

    update: updated movie component

--- src/components/main/movie/Movie.tsx
+++ src/components/main/movie/Movie.tsx
@@ -1,26 +1,26 @@
 import React, {useState} from 'react';
 import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
+    AppBar,
     Avatar,
     Box,
     CardMedia,
     Chip,
     Container,
-    ListItem,
     List,
+    ListItem,
     ListItemAvatar,
     ListItemText,
     makeStyles,
+    Tab,
+    Tabs,
     Theme,
-    Typography, AppBar, Tabs, Tab
+    Typography
 } from "@material-ui/core";
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import {FixedSizeList} from 'react-window';
 import ImageIcon from '@material-ui/icons/Image';
 import CastItem from "./CastItem";
 import ImageCard from "./ImageCard";
+import {a11yProps, TabPanel} from "../main";
+import Divider from "@material-ui/core/Divider";
 
 const IMAGES_URL = 'https://image.tmdb.org/t/p/w500';
 
@@ -4135,55 +4135,37 @@ const images = {
     ]
 }
 
-type CreditsType = {
-    id: number,
-    cast: {}[]
-}
-
-interface ChipData {
-    text: string
-}
-
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-    const {children, value, index, ...other} = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`scrollable-auto-tabpanel-${index}`}
-            aria-labelledby={`scrollable-auto-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box>
-                    {children}
-                </Box>
-            )}
-        </div>
-    );
-}
-
-function a11yProps(index: any) {
-    return {
-        id: `scrollable-auto-tab-${index}`,
-        'aria-controls': `scrollable-auto-tabpanel-${index}`,
-    };
-}
-
 function Movie() {
     const useStyles = makeStyles((theme: Theme) => ({
         root: {
             backgroundColor: theme.palette.primary.main,
+            display: 'flex',
+            backgroundImage: `url(${IMAGES_URL}${object.backdrop_path})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            // zIndex: theme.zIndex.drawer + 1,
+            color: '#fff',
+        },
+        vote: {
+            display: 'flex',
+        },
+        vote_average: {
+            fontWeight: 800,
+            paddingRight: 10,
+            color: 'green'
+        },
+        vote_count: {},
+        movieImage: {
+            width: 250,
+            height: 300,
+        },
+        divider: {
+            marginRight: 30,
+            marginLeft: 30,
         },
         customList: {
             display: 'flex',
+            overflowX: 'scroll'
         }, listItem: {
             flexDirection: 'column',
         }, actionArea: {
@@ -4229,45 +4211,66 @@ function Movie() {
     };
 
     return (
-        <Container maxWidth="lg">
-            <Box className={classes.root} component={'div'}>
-                <CardMedia component={'img'} image={`${IMAGES_URL}${object.backdrop_path}`}/>
-                {/*<CardMedia component={'img'} image={`${IMAGES_URL}${object.poster_path}`}/>*/}
-                <Typography component='h1' gutterBottom>Name: {object.title}</Typography>
-                <Typography component='div' gutterBottom>Genre: {object.genres.map(g => g.name).join()}</Typography>
-                <Typography component='div' gutterBottom>Rating: {object.vote_average}/10</Typography>
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel2a-content"
-                        id="panel2a-header"
-                    >
-                        <Typography>Overview</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
+        <Container maxWidth="xl">
+            <Box component={'div'}>
+                <Box component={'div'} className={classes.root}>
+                    <Box component={'div'}>
+                        <CardMedia className={classes.movieImage} component={'img'}
+                                   image={`${IMAGES_URL}${object.poster_path}`}/>
+                    </Box>
+                    <Divider className={classes.divider} orientation="vertical" flexItem/>
+                    <Box component={'div'}>
+                        <Box component={'div'}>
+                            <Typography component='h1' gutterBottom>{object.title}</Typography>
+                            <Typography component='h3'
+                                        gutterBottom>{object.release_date} ({object.production_countries.map((pc) => pc.iso_3166_1).join()})
+                            </Typography>
+                            <Typography component='div'
+                                        gutterBottom>{object.genres.map(g => g.name).join()}</Typography>
+                            <Typography
+                                component='div'
+                                gutterBottom>{object.runtime} min</Typography>
+                            <Typography
+                                component='div'
+                                gutterBottom>{object.status}</Typography>
+                        </Box>
+                        <Box component={'div'} className={classes.vote}>
+                            <Typography className={classes.vote_average} component='div'
+                                        gutterBottom>{object.vote_average}</Typography>
+                            <Typography className={classes.vote_count} component='div'
+                                        gutterBottom>{object.vote_count}</Typography>
+                        </Box>
+                        <Typography component='h2' gutterBottom>{object.tagline}</Typography>
                         <Typography component='p' gutterBottom>{object.overview}</Typography>
-                    </AccordionDetails>
-                </Accordion>
-                <List component={'ul'} className={classes.customList}>
-                    Cast: {credits.cast.map((c, i) =>
-                    <ListItem key={i} className={classes.listItem}>
-                        <CastItem classes={classes} image={c.profile_path ? `${IMAGES_URL}${c.profile_path}` : ''}
-                                  title={c.name}
-                                  subtitle={c.character}/>
-                    </ListItem>
-                )}
-                </List>
-                <List component={'ul'} className={classes.customList}>
-                    Production companies: {object.production_companies.map((pc, i) =>
-                    <ListItem key={i} className={classes.listItem}>
-                        <ListItemAvatar>
-                            {pc.logo_path ? <Avatar src={`${IMAGES_URL}${pc.logo_path}`}/> : <ImageIcon/>}
-                        </ListItemAvatar>
-                        <ListItemText primary={`${pc.name}`} secondary={`${pc.origin_country}`}/>
-                    </ListItem>
-                )}
-                </List>
-                <Typography component={'div'} gutterBottom>
+                    </Box>
+                </Box>
+                <Box component={'div'}>
+                    Cast:
+                    <List className={classes.customList}>
+                        {credits.cast.map((c, i) =>
+                            <ListItem key={i} className={classes.listItem}>
+                                <CastItem classes={classes}
+                                          image={c.profile_path ? `${IMAGES_URL}${c.profile_path}` : ''}
+                                          title={c.name}
+                                          subtitle={c.character}/>
+                            </ListItem>
+                        )}
+                    </List>
+                </Box>
+                <Box component={'div'}>
+                    Production companies:
+                    <List className={classes.customList}>
+                        {object.production_companies.map((pc, i) =>
+                            <ListItem key={i} className={classes.listItem}>
+                                <ListItemAvatar>
+                                    {pc.logo_path ? <Avatar src={`${IMAGES_URL}${pc.logo_path}`}/> : <ImageIcon/>}
+                                </ListItemAvatar>
+                                <ListItemText primary={`${pc.name}`} secondary={`${pc.origin_country}`}/>
+                            </ListItem>
+                        )}
+                    </List>
+                </Box>
+                <Box component={'div'}>
                     Media:
                     <AppBar position="static" color="default">
                         <Tabs
@@ -4286,35 +4289,38 @@ function Movie() {
                     <TabPanel value={value} index={0}>
                         Backdrops:
                         <List component={'ul'} className={classes.customList}>
-                            {images.backdrops.map(b => <ImageCard classes={classes} aspect_ratio={b.aspect_ratio}
-                                                                  file_path={b.file_path ? `${IMAGES_URL}${b.file_path}` : ''}
-                                                                  height={b.height}
-                                                                  vote_average={b.vote_average}
-                                                                  vote_count={b.vote_count}
-                                                                  width={b.width}/>)}
+                            {images.backdrops.map((b, i) => <ImageCard key={i}
+                                                                       classes={classes}
+                                                                       aspect_ratio={b.aspect_ratio}
+                                                                       file_path={b.file_path ? `${IMAGES_URL}${b.file_path}` : ''}
+                                                                       height={b.height}
+                                                                       vote_average={b.vote_average}
+                                                                       vote_count={b.vote_count}
+                                                                       width={b.width}/>)}
                         </List>
                     </TabPanel>
                     <TabPanel value={value} index={1}>
                         Posters:
                         <List component={'ul'} className={classes.customList}>
-                            {images.posters.map(b => <ImageCard classes={classes} aspect_ratio={b.aspect_ratio}
-                                                                file_path={b.file_path ? `${IMAGES_URL}${b.file_path}` : ''}
-                                                                height={b.height}
-                                                                vote_average={b.vote_average}
-                                                                vote_count={b.vote_count}
-                                                                width={b.width}/>)}
+                            {images.posters.map((p, i) => <ImageCard key={i} classes={classes}
+                                                                     aspect_ratio={p.aspect_ratio}
+                                                                     file_path={p.file_path ? `${IMAGES_URL}${p.file_path}` : ''}
+                                                                     height={p.height}
+                                                                     vote_average={p.vote_average}
+                                                                     vote_count={p.vote_count}
+                                                                     width={p.width}/>)}
                         </List>
                     </TabPanel>
-                </Typography>
+                </Box>
 
-                <Typography component={'div'} gutterBottom>
+                <Box component={'div'}>
                     Keywords: {keyWords.keywords.map(kw => kw.name)
                     .map((text, i) =>
                         <Chip size={'small'}
                               color={'secondary'} key={i}
                               label={text}
                               onClick={(e) => handleClick(e, text)}/>)}
-                </Typography>
+                </Box>
             </Box>
         </Container>
     );
